f2f0a1b1b99756d6e5fd3b1eb5eb74ede4d0abc3
Hosea <karanja.mungai23@gmail.com>
2025-04-27 18:05:19 +0300

    minor fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { TableModule } from 'primeng/table';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent, MenuComponent, TableModule],
   template: `
     <section>
       <app-header></app-header>
+      
+      <p-table [value]="products" [tableStyle]="{ 'min-width': '50rem' }">
+  <ng-template pTemplate="header">
+      <tr>
+          <th>Code</th>
+          <th>Name</th>
+          <th>Category</th>
+          <th>Quantity</th>
+      </tr>
+  </ng-template>
+  
+  <ng-template pTemplate="body" let-product>
+      <tr>
+          <td>{{ product.code }}</td>
+          <td>{{ product.name }}</td>
+          <td>{{ product.category }}</td>
+          <td>{{ product.quantity }}</td>
+      </tr>
+  </ng-template>
+</p-table>
+
       <router-outlet></router-outlet>
     </section>
   `,
@@ -14,4 +37,8 @@ import { HeaderComponent } from './header/header.component';
 })
 export class AppComponent {
   title = 'AngularPractice';
+  products: any[] = [
+    { code: 'f230fh0g3', name: 'Leaf Rake', category: 'Garden', quantity: 12 },
+    { code: 'hv34j5', name: 'Saw', category: 'Tools', quantity: 20 },
+  ];
 }
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  imports: [],
+  template: `
+    <div class="men">
+    </div>
+  `,
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
